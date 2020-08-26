dadc97e339ddba9ac636961851d9efea9ebfd4ed
17293013 <D5i4m3a2s1>
2020-08-26 10:59:45 +0300

    Updates

---
+++ precache-manifest.73bbf1e9a072a282059026190673a0ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23da6410abc0992ae5786c63ec251702",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "67a196f4561d5d5c8692",
+    "url": "/react-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/react-page/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a196f4561d5d5c8692",
+    "url": "/react-page/static/js/main.db84adc9.chunk.js"
+  },
+  {
+    "revision": "01a51c07463b9a2cfdf3",
+    "url": "/react-page/static/js/runtime-main.93f71819.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
